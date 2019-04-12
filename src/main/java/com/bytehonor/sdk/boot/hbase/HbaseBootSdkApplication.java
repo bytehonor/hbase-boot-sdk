c19f68d399ee3c3f069fbc65be7b7c5306827e02
lijianqiang <newphper@gmail.com>
2019-04-12 16:26:32 +0800

    init

---
+++ src/main/java/com/bytehonor/sdk/boot/hbase/HbaseBootSdkApplication.java
@@ -0,0 +1,13 @@
+package com.bytehonor.sdk.boot.hbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbaseBootSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbaseBootSdkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bytehonor/sdk/boot/hbase/HbaseBootSdkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bytehonor.sdk.boot.hbase;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HbaseBootSdkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
